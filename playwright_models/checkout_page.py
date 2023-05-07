e92ccd45f145b36681611c12714548e777eb8d82
ukk0 <sami.ukk91@gmail.com>
2023-05-07 17:48:40 +0200

    Checkout page first test

--- playwright_models/checkout_page.py
+++ playwright_models/checkout_page.py
@@ -3,3 +3,26 @@ class CheckoutPage:
         self.page = page
         self.playwright = playwright
         playwright.selectors.set_test_id_attribute("data-test")
+
+        self.submit_info_button = page.get_by_test_id("continue")
+        self.input_first_name = page.get_by_placeholder("First Name")
+        self.input_last_name = page.get_by_placeholder("Last Name")
+        self.input_zip_code = page.get_by_placeholder("Zip/Postal Code")
+        self.error_missing_info = page.get_by_test_id("error")
+
+    def fill_first_name(self, first_name: str):
+        self.input_first_name.fill(first_name)
+
+    def fill_last_name(self, last_name: str):
+        self.input_last_name.fill(last_name)
+
+    def fill_zip_code(self, zip_code: str):
+        self.input_zip_code.fill(zip_code)
+
+    def proceed_to_final_checkout(self):
+        self.submit_info_button.click()
+        self.page.wait_for_load_state()
+
+    def missing_info_warning(self):
+        error_msg = self.error_missing_info.inner_text()
+        return error_msg
--- tests/test_checkout_page.py
+++ tests/test_checkout_page.py
@@ -10,7 +10,28 @@ def test_payer_info_is_required_at_checkout(page, playwright, login_cookie, cart
     page.goto(url="https://www.saucedemo.com/checkout-step-one.html")
 
     # Try to proceed from the page without filling in required information
+    check.proceed_to_final_checkout()
+    assert check.missing_info_warning() == "Error: First Name is required"
+    check.fill_first_name(first_name="FIRST")
+
+    check.proceed_to_final_checkout()
+    assert check.missing_info_warning() == "Error: Last Name is required"
+    check.fill_last_name(last_name="LAST")
+
+    check.proceed_to_final_checkout()
+    assert check.missing_info_warning() == "Error: Postal Code is required"
+    check.fill_zip_code(zip_code="123456")
+
+    # Proceed successfully to final checkout page
+    check.proceed_to_final_checkout()
+    page.expect_navigation(url="https://www.saucedemo.com/checkout-step-two.html")
 
 
-def test_item_prices_match_total():
-    pass
+def test_item_prices_match_total(page, playwright, login_cookie, cart_fill_script):
+    check = CheckoutPage(page, playwright)
+
+    # Skip login and payment info and navigate to final checkout with a pre-filled shopping cart
+    page.context.add_cookies([login_cookie])
+    page.context.add_init_script(cart_fill_script)
+    page.goto(url="https://www.saucedemo.com/checkout-step-two.html")
+
